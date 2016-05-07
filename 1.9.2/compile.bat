8f73eed29d278e7951d345d316a0a517f7fa3789
Alexander Xia <Alexander Xia>
2016-05-07 19:41:32 +0800

    Fix data folder url decoding bug

--- 1.9.2/compile.bat
+++ 1.9.2/compile.bat
@@ -1,6 +1,6 @@
 @echo off
 SET MC_VERSION=1.9.2
-SET CSL_VERSION=13.6
+SET CSL_VERSION=13.7a
 cd src
 javac -source 1.6 -target 1.6 -d ../bin -cp ;../lib/commons-codec-1.9.jar;../lib/commons-io-2.4.jar;../lib/gson-2.2.4.jar;../lib/guava-17.0.jar;../lib/authlib-1.5.22.jar;../%MC_VERSION%.jar *.java
 javac -source 1.6 -target 1.6 -d ../bin -cp ;../lib/commons-io-2.4.jar;../lib/launchwrapper-1.7.jar customskinloader/tweaker/*.java
--- 1.9.2/src/customskinloader/utils/MinecraftUtil.java
+++ 1.9.2/src/customskinloader/utils/MinecraftUtil.java
@@ -3,6 +3,7 @@ package customskinloader.utils;
 import java.io.File;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.net.URLDecoder;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -45,21 +46,23 @@ public class MinecraftUtil {
 	}
 	
 	private final static Pattern MINECRAFT_CORE_FILE_PATTERN = Pattern.compile("^(.*?)/versions/(.*?)/(.*?).jar$");
-	public static void testProbe(){
+	private static void testProbe(){
 		URLClassLoader ucl = (URLClassLoader)new MinecraftUtil().getClass().getClassLoader();
 		URL urls[] = ucl.getURLs();
 		for(URL url:urls){
-			Matcher m = MINECRAFT_CORE_FILE_PATTERN.matcher(url.getPath());
+			Matcher m = null;
+			try{
+				m=MINECRAFT_CORE_FILE_PATTERN.matcher(URLDecoder.decode(url.getPath(),"UTF-8"));
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			if(m==null)
+				continue;
 			if(!m.matches())
 				continue;
-			if(minecraftDataFolder==null){
-				try{
-					minecraftDataFolder=new File(url.getPath()).getParentFile().getParentFile().getParentFile();
-				}catch(Exception e){
-					minecraftDataFolder=null;
-				}
-			}
-			System.out.println(url.getPath()+" "+minecraftDataFolder.getAbsolutePath());
+			if(minecraftDataFolder==null)
+				minecraftDataFolder=new File(m.group(1));
+			//System.out.println(url.getPath()+" "+minecraftDataFolder.getAbsolutePath());
 			if(minecraftVersion==null)
 				minecraftVersion=m.group(2);
 			break;
