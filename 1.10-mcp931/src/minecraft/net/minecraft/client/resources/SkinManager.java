87a40fc3c7415773bcc399957500e4b2517d3ba5
xfl03 <xfl03@hotmail.com>
2016-10-18 22:12:06 +0800

    Fix auto clean

--- 1.10-mcp931/src/minecraft/net/minecraft/client/resources/SkinManager.java
+++ 1.10-mcp931/src/minecraft/net/minecraft/client/resources/SkinManager.java
@@ -48,6 +48,7 @@ public class SkinManager
         //CustomSkinLoader Begin (Init)
         customskinloader.loader.MojangAPILoader.defaultSessionService=sessionService;
         customskinloader.utils.HttpTextureUtil.defaultCacheDir=skinCacheDirectory;
+        customskinloader.utils.HttpTextureUtil.initDefaultCacheDir();
         //CustomSkinLoader End
     }
 
--- 1.8-mcp910/src/minecraft/net/minecraft/client/resources/SkinManager.java
+++ 1.8-mcp910/src/minecraft/net/minecraft/client/resources/SkinManager.java
@@ -54,6 +54,7 @@ public class SkinManager
         //CustomSkinLoader Begin (Init)
         customskinloader.loader.MojangAPILoader.defaultSessionService=sessionService;
         customskinloader.utils.HttpTextureUtil.defaultCacheDir=skinCacheDirectory;
+        customskinloader.utils.HttpTextureUtil.initDefaultCacheDir();
         //CustomSkinLoader End
     }
 
