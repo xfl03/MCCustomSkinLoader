481b0d9df91d670dd74b26bb4e260341152c5ebb
xfl03 <xfl03@hotmail.com>
2016-12-04 14:38:17 +0800

    Remove 1.7.10

---
+++ Forge/source/customskinloader/forge/FMLMod.java
@@ -0,0 +1,7 @@
+package customskinloader.forge;
+
+
+@Mod(modid = "customskinloader", name = "CustomSkinLoader", version = "@MOD_FULL_VERSION@", clientSideOnly=true)
+public class FMLMod{
+    
+}
---
+++ Forge/source/customskinloader/forge/FMLPlugin.java
@@ -0,0 +1,33 @@
+package customskinloader.forge;
+
+import java.util.Map;
+
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+
+public class FMLPlugin implements IFMLLoadingPlugin {
+
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[] {customskinloader.tweaker.ClassTransformer.class.getName()};
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+
+}
