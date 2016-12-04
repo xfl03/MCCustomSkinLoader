79440740ee46dabcf9912edf780356c8fb004a8b
xfl03 <xfl03@hotmail.com>
2016-12-04 14:50:36 +0800

    Forge Test

--- Forge/source/customskinloader/forge/FMLMod.java
+++ Forge/source/customskinloader/forge/FMLMod.java
@@ -1,5 +1,6 @@
 package customskinloader.forge;
 
+import net.minecraftforge.fml.common.Mod;
 
 @Mod(modid = "customskinloader", name = "CustomSkinLoader", version = "@MOD_FULL_VERSION@", clientSideOnly=true)
 public class FMLMod{
