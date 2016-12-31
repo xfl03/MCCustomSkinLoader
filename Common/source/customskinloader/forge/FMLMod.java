8a1dca0dd414c9208944af4918bc07481eedfa22
xfl03 <xfl03@hotmail.com>
2016-12-31 21:54:28 +0800

    May fix forge

--- Common/source/customskinloader/forge/FMLMod.java
+++
@@ -1,8 +0,0 @@
-package customskinloader.forge;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod(modid = "customskinloader", name = "CustomSkinLoader", version = "@MOD_FULL_VERSION@", clientSideOnly=true)
-public class FMLMod{
-    
-}
