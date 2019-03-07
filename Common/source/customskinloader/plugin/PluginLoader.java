92446c1399ec96d331d25f6a35b45bd0168f1407
ZekerZhayard <trees1000@qq.com>
2019-03-07 22:49:52 +0800

    Remove PluginClassLoader.

--- Common/source/customskinloader/plugin/PluginLoader.java
+++ Common/source/customskinloader/plugin/PluginLoader.java
@@ -2,43 +2,33 @@ package customskinloader.plugin;
 
 import java.io.File;
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.ServiceLoader;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.filefilter.AbstractFileFilter;
-
-import com.google.common.collect.Maps;
 
 import customskinloader.CustomSkinLoader;
 import customskinloader.loader.ProfileLoader;
 
 public class PluginLoader {
-    private static PluginClassLoader pcl = new PluginClassLoader(((URLClassLoader) PluginLoader.class.getClassLoader()).getURLs(), PluginLoader.class.getClassLoader());
-    
     public static HashMap<String, ProfileLoader.IProfileLoader> loadPlugins() {
         File pluginsDir = new File(CustomSkinLoader.DATA_DIR, "Plugins");
+        ArrayList<URL> urls = new ArrayList<URL>();
         if (!pluginsDir.isDirectory()) {
             pluginsDir.mkdirs();
         } else {
-            for (File plugin : FileUtils.listFiles(pluginsDir, new AbstractFileFilter() {
-                @Override
-                public boolean accept(final File file) {
-                    String fileName = file.getName().toLowerCase();
-                    return fileName.endsWith(".jar") || fileName.endsWith(".zip");
-                }
-            }, null)) {
+            for (File plugin : FileUtils.listFiles(pluginsDir, new String[] {"jar", "zip"}, false)) {
                 try {
-                    pcl.addURL(plugin.toURI().toURL());
-                    CustomSkinLoader.logger.info("Successfully to load file: " + plugin.getName());
-                } catch (MalformedURLException e) {
-                    CustomSkinLoader.logger.warning("Could not load file: " + plugin.getName());
-                }
+                    urls.add(plugin.toURI().toURL());
+                    CustomSkinLoader.logger.info("Found a jar or zip file: " + plugin.getName());
+                } catch (MalformedURLException ignored) {}
             }
         }
-        ServiceLoader<ICustomSkinLoaderPlugin> sl = ServiceLoader.load(ICustomSkinLoaderPlugin.class, pcl);
-        HashMap<String, ProfileLoader.IProfileLoader> profileLoaders = Maps.newHashMap();
+        ServiceLoader<ICustomSkinLoaderPlugin> sl = ServiceLoader.load(ICustomSkinLoaderPlugin.class, new URLClassLoader(urls.toArray(new URL[0]), PluginLoader.class.getClassLoader()));
+        HashMap<String, ProfileLoader.IProfileLoader> profileLoaders = new HashMap<String, ProfileLoader.IProfileLoader>();
         for (ICustomSkinLoaderPlugin cslPlugin : sl) {
             ProfileLoader.IProfileLoader profileLoader = cslPlugin.getProfileLoader();
             profileLoaders.put(profileLoader.getName().toLowerCase(), profileLoader);
