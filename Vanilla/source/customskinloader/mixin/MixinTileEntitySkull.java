04899ff6764fba6f02e3909e857c0cc6d864bc82
ZekerZhayard <trees1000@qq.com>
2021-05-29 18:53:27 +0800

    Support Fabric 1.17-pre1. (#170)

--- Vanilla/source/customskinloader/mixin/MixinTileEntitySkull.java
+++ Vanilla/source/customskinloader/mixin/MixinTileEntitySkull.java
@@ -8,6 +8,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+/** Fix https://bugs.mojang.com/browse/MC-65587 */
 @Mixin(TileEntitySkull.class)
 public abstract class MixinTileEntitySkull {
     @Inject(
