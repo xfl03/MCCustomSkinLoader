dc5ecfb576ad509d4c21d4c698ecefd6f20a57cf
ZekerZhayard <trees1000@qq.com>
2021-06-09 14:55:20 +0800

    Support Vanilla and Fabric 1.17. Remove Vanilla 1.15.2. (#171)
    * Support Fabric 1.17-rc2.
    
    * Support Vanilla and Fabric 1.17. Remove Vanilla 1.15.2.

---
+++ Vanilla/source/customskinloader/mixin/MixinThreadDownloadImageDataV3.java
@@ -0,0 +1,32 @@
+package customskinloader.mixin;
+
+import customskinloader.fake.FakeSkinBuffer;
+import net.minecraft.client.renderer.ThreadDownloadImageData;
+import net.minecraft.client.renderer.texture.NativeImage;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ThreadDownloadImageData.class) // 1.17-rc2+
+@SuppressWarnings("target")
+public abstract class MixinThreadDownloadImageDataV3 {
+    @Final
+    @Shadow
+    private Runnable processTask;
+
+    @Shadow
+    abstract NativeImage processLegacySkin(NativeImage nativeImageIn);
+
+    @Redirect(
+        method = "Lnet/minecraft/client/renderer/ThreadDownloadImageData;loadTexture(Ljava/io/InputStream;)Lnet/minecraft/client/renderer/texture/NativeImage;",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/renderer/ThreadDownloadImageData;processLegacySkin(Lnet/minecraft/client/renderer/texture/NativeImage;)Lnet/minecraft/client/renderer/texture/NativeImage;"
+        )
+    )
+    private NativeImage redirect_loadTexture(ThreadDownloadImageData _this, NativeImage image) {
+        return FakeSkinBuffer.processLegacySkin(image, this.processTask, this::processLegacySkin);
+    }
+}
