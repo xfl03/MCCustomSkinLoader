b3d9e18ec9e580370ae16a49332706dab7ea41c9
ZekerZhayard <trees1000@qq.com>
2022-06-03 07:59:18 +0800

    Support 1.19, Remove Vanilla 1.17.1

--- Common/source/customskinloader/fake/itf/IFakeIResource.java
+++ Common/source/customskinloader/fake/itf/IFakeIResource.java
@@ -6,8 +6,17 @@ import net.minecraft.client.resources.IResource;
 
 /** {@link net.minecraft.client.resources.IResource} is no longer an interface since 22w14a */
 public interface IFakeIResource {
-    // 1.13.2+
-    default InputStream func_199027_b() {
-        return ((IResource) this).getInputStream();
+    // 1.13.2 ~ 22w13a
+    interface V1 {
+        default InputStream func_199027_b() {
+            return ((IResource) this).getInputStream();
+        }
+    }
+
+    // 22w14a+
+    interface V2 {
+        default InputStream open() {
+            return ((IFakeIResource.V1) this).func_199027_b();
+        }
     }
 }
